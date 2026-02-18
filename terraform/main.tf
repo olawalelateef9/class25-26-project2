9811be38bbe3af0ab745cfc055ac27c71c756b66
olawale lateef <olawalelateef9@gmail.com>
2026-02-17 13:22:12 -1200

    terraform code corrected

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket  = "olawale-s3-devops-bucket"
-    key     = "envs/dev/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
-  }
-  required_version = ">= 1.6.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 # --- 1. TERRAFORM CONFIGURATION ---
 terraform {
